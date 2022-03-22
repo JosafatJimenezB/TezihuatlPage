3b8bcf97f0d19593647c4ad799007d862be3e242
JosafatJimenezB <josa221001@gmail.com>
2022-03-21 18:02:24 -0600

    add: pagina web

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//Swiper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+  });
+
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+//Navigation bar effects on scroll
+
+document.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//Responsive navigation menu toggle
+
+const menuBtn = document.querySelector('.nav-menu-btn');
+const closeBtn = document.querySelector('.nav-close-btn');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener("click", () =>{
+  navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+  navigation.classList.remove("active");
+});
+
+
+
+
