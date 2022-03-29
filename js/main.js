c2ed69a61878cf29db15ec1b3101854c7083babd
JosafatJimenezB <josa221001@gmail.com>
2022-03-28 21:09:30 -0600

    add: aterminando la seccion de las resñas y añadiendo el responsive

--- js/main.js
+++ js/main.js
@@ -45,3 +45,22 @@ closeBtn.addEventListener("click", () =>{
   navigation.classList.remove("active");
 });
 
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
