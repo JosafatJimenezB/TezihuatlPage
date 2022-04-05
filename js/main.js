22d9297c7d70b344fa0ef21958d4a3978437d248
JosafatJimenezB <josa221001@gmail.com>
2022-04-05 10:36:54 -0500

    add: aloader a la pagina

--- js/main.js
+++ js/main.js
@@ -165,3 +165,14 @@ function alerta() {
 // }).then(
 // message => alerta(message)
 // );
+
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
